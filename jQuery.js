ca386620a1616164ffafe98fe5f112216628ae4a
Rodion Raskolnikov <zowen11@gmail.com>
2015-09-15 23:06:53 -0400

    Update 2015-09-15
    Implemented Disqus commenting system. Other small changes. More to come!

--- jQuery.js
+++ jQuery.js
@@ -1,14 +1,24 @@
 $(document).ready(function()
 {
 	$(".button, .button_square, .button_rect, padded_image").mouseenter(function()
-		{
-			$(this).fadeTo(100, 0.75);
-		});
+	{
+		$(this).fadeTo(100, 0.75);
+	});
 	$(".button, .button_square, .button_rect, padded_image").mouseleave(function()
-		{
-			$(this).fadeTo(100, 1);
-		});
+	{
+		$(this).fadeTo(100, 1);
+	});
 	$("#PFtabs").tabs();
 	$("#menu, #menu2").accordion({collapsible: true, active: false, heightStyle: "content"});
+	$("#physicsHeader").load("physicsHeader.html");
+	$("#footer").load("footer.html");
+	$("#calc").load("calc.html");
+	$("#comments").html("[+] Show comments");
+	$("#comments").on("click", function()
+	{
+		var disqus_shortname = "wondergrad";
+		$.ajax({type: "GET", url: "http://" + disqus_shortname + ".disqus.com/embed.js", dataType: "script"});
+       	$(this).fadeOut();
+	});
 });
 
--- lastModified.js
+++ lastModified.js
@@ -1,18 +1,2 @@
-var months = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."];
-var lm = new Date(document.lastModified);
-var min = lm.getMinutes();
-	if (min < 10)
-	{
-		min = "0" + min;
-	}
-var h = lm.getHours();
-	if (h < 10)
-	{
-		h = "0" + h;
-	}
-var d = lm.getDate();
-var m = lm.getMonth();
-	m = months[m];
-var y = lm.getFullYear();
-
-document.write("Designed for Chrome • Typeset in Roboto • Last modified " + d + " " + m + " " + y);
+var date = new Date(document.lastModified);
+document.getElementById("date").innerHTML = date.getMonth() + 1 + "/" + date.getDate();
